cbb90df7e34a118f110517179583676c1a8cd696
future <sgmr0408@gmail.com>
2021-12-13 05:31:00 -0700

    prepared statements

---
+++ create.py
@@ -0,0 +1,13 @@
+import sqlite3
+# connect = sqlite3.connect('data.db')
+with sqlite3.connect('data.db') as connect:
+
+    cursor = connect.cursor()
+
+    title = input('Title : ')
+    body = input('Body : ')
+
+    sql = 'INSERT INTO topics (title, body) VALUES(?,?)'
+    cursor.execute(sql, (title, body))
+
+    connect.commit()
